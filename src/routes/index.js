bcd293e5846e3a77f39bbc93b48eed6ea866dcb1
EdueliasXez <edueliasxez@gmail.com>
2023-10-06 00:53:51 -0300

    soft delete

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const events_router = require('./events_router');
 const reviews_router = require('./reviews_router');
 const users_router = require('./users_router');
 const google_router = require('./google_router');
+const soft_delete_router = require('./soft_delete_router');
 const router = Router();
 
 
@@ -10,6 +11,7 @@ router.use('/events', events_router);
 router.use('/review', reviews_router);
 router.use('/user', users_router);
 router.use('/google', google_router);
+router.use('/soft_delete', soft_delete_router);
 
 
 router.use((req, res, next) => {
