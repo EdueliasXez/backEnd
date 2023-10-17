ab8c08c48ec2b2553595b479dc6360f12672fedf
JulianaCelis <juliana0celis@email.com>
2023-10-17 09:54:42 -0500

    manejo de tickets

--- src/routes/users_router.js
+++ src/routes/users_router.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const userRouter = express.Router();
 const { getUsers, registerUser, loginUser, getUsersById} = require('../controllers/index');
+const {Ticket} = require('../db');
 const { authenticateToken } = require('../middlewares/authMiddleware.js');
 
 userRouter.get('/', getUsers);
 
 userRouter.get('/:userId', getUsersById);
+
+userRouter.get('/tickets/:userId', async (req, res) => {
+    const userId = req.params.userId;
+  
+    try {
+      const tickets = await Ticket.find({ userId }); 
+      res.json(tickets);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error al obtener los tickets.' });
+    }
+  });
+  
 
 userRouter.post('/register', registerUser);
 
